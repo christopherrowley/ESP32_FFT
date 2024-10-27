c804004f6fd4dcafdfcca9ae3a8f0679bd6b3603
christopherrowley <chris_rowley18@hotmail.com>
2024-10-26 22:47:52 -0400

    made strobe light
    halloween feature

---
+++ ExampleApps/strobeLight/strobeLight.ino
@@ -0,0 +1,113 @@
+
+#include <FastLED.h> // Code for LEDs
+// Strobe light designed for Halloween with repetitive flashes of red added in. 
+
+
+/* initialize Hardware Variables */
+#define LED_PIN           4 // Where LED strip is connected to board
+#define NUM_LEDS         256
+#define COLOR_ORDER     GRB           // If colours look wrong, play with this
+#define CHIPSET         WS2812B       // LED strip type
+#define MAX_MILLIAMPS   2000          // Careful with the amount of power here if running off USB port
+const int BRIGHTNESS_SETTINGS =150;  // Configureable brightness. If power draw is an issue, lower value
+#define LED_VOLTS       5             // Usually 5 or 12
+
+
+// Lighting timing specific
+#define TIME_LIGHT_ON         175 // milliseconds
+#define TIME_LIGHT_OFF        175 // milliseconds
+#define REPEAT_C1    10 // Times to repeat colour 1
+#define REPEAT_C2    2 // Times to repeat colour 1
+int c1_counter = 0;
+int c2_counter = 0;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+
+
+/* setup memory block for the leds */
+CRGB leds[NUM_LEDS];
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(LED_VOLTS, MAX_MILLIAMPS);
+  FastLED.setBrightness(BRIGHTNESS_SETTINGS);
+
+  Serial.println("\nStarting strobe light...");
+
+  /* Setup Pin for LED data export */
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+
+  // Loop over first colour
+  for (int lc = 0; lc < REPEAT_C1; lc++){
+
+    // Start by turning light on white: 
+    for (int i = 0; i < NUM_LEDS; i++)  {
+      leds[i] = CRGB(250, 250, 250);
+    }
+    delay(5);
+    FastLED.show();
+
+    currentMillis = millis();
+    // wait to keep the light on for the interval
+    while(currentMillis - previousMillis < TIME_LIGHT_ON)   {
+      currentMillis = millis();
+    }
+
+    // then turn light off.
+    FastLED.clear();
+    FastLED.show();
+    previousMillis = currentMillis; 
+
+    // keep the light off for the interval
+    currentMillis = millis();
+
+    // Then wait until its been off long enough.
+    while(currentMillis - previousMillis < TIME_LIGHT_OFF)   {
+      currentMillis = millis();
+    }
+    previousMillis = currentMillis; 
+  }
+
+// Loop over second colour
+  for (int lc = 0; lc < REPEAT_C2; lc++){
+
+    // Start by turning light on white: 
+    for (int i = 0; i < NUM_LEDS; i++)  {
+      leds[i] = CRGB(250, 0, 0);
+    }
+
+    delay(5);
+    FastLED.show();
+
+    currentMillis = millis();
+    // wait to keep the light on for the interval
+    while(currentMillis - previousMillis < TIME_LIGHT_ON)   {
+      currentMillis = millis();
+    }
+
+    // then turn light off.
+    FastLED.clear();
+    FastLED.show();
+    previousMillis = currentMillis; 
+
+    // keep the light off for the interval
+    currentMillis = millis();
+
+    // Then wait until its been off long enough.
+    while(currentMillis - previousMillis < TIME_LIGHT_OFF)   {
+      currentMillis = millis();
+    }
+    previousMillis = currentMillis; 
+  } // End loop colour 2. 
+
+}
+
+
--- Intro2ESP32/Intro2ESP32.md
+++ Intro2ESP32/Intro2ESP32.md
@@ -8,8 +8,18 @@ Here we will get an understanding of the FastLED programming package, as well as
 
 
 
-## Coding Steps:
-1. Start by uploading W1_spectrumAnalyzer.ino to the ESP32 with all the default values. You may need to check that you have the right COM port selected. Hold down the boot button on the ESP32 while uploading. 
+## BASICS
+We break up arduino code into 3 main blocks:
+1. Define all the necessary variables that the code needs to run (e.g. pin locations for connected hardware)
+2. The 'setup' function - which tells the chip what to run at the very beginning to get things set up for the main function
+3. The 'loop' function - which contains the code that you plan to run continuously 
+
+## Datatypes
+
+## Hello World https://arduinogetstarted.com/tutorials/arduino-hello-world
+
+
+
 
 
 
@@ -19,6 +29,61 @@ FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds, kMatrixWidth, kMatrixHei
   NEO_MATRIX_TOP        + NEO_MATRIX_LEFT +
   NEO_MATRIX_ROWS       + NEO_MATRIX_ZIGZAG +
   NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS);
+
+
+## LED Matrix Basics:
+Draw a pixel in solid white
+>>  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
+
+ // fix the screen with green
+  matrix.fillRect(0, 0, 32, 16, matrix.Color333(0, 7, 0));
+
+// draw a box in yellow
+  matrix.drawRect(0, 0, 32, 16, matrix.Color333(7, 7, 0));
+
+// draw an 'X' in red
+  matrix.drawLine(0, 0, 31, 15, matrix.Color333(7, 0, 0));
+  matrix.drawLine(31, 0, 0, 15, matrix.Color333(7, 0, 0));
+
+// draw a blue circle
+  matrix.drawCircle(7, 7, 7, matrix.Color333(0, 0, 7));
+ 
+  // fill a violet circle
+  matrix.fillCircle(23, 7, 7, matrix.Color333(7, 0, 7));
+
+  // fill the screen with 'black'
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+
+
+## Writing on the screen:
+There is a matrix print option which is similar to serial print.  But we first need to set where our cursor will be. 
+// draw some text!
+  matrix.setCursor(1, 0);   // start at top left, with one pixel of spacing
+  matrix.setTextSize(1);    // size 1 == 8 pixels high
+ 
+  // print each letter with a rainbow color
+  matrix.setTextColor(matrix.Color333(7,0,0));
+  matrix.print('1');
+  matrix.setTextColor(matrix.Color333(7,4,0)); 
+  matrix.print('6');
+  matrix.setTextColor(matrix.Color333(7,7,0));
+  matrix.print('x');
+  matrix.setTextColor(matrix.Color333(4,7,0)); 
+  matrix.print('3');
+  matrix.setTextColor(matrix.Color333(0,7,0));  
+  matrix.print('2');
+ 
+  matrix.setCursor(1, 9);   // next line
+  matrix.setTextColor(matrix.Color333(0,7,7)); 
+  matrix.print('*');
+  matrix.setTextColor(matrix.Color333(0,4,7)); 
+  matrix.print('R');
+  matrix.setTextColor(matrix.Color333(0,0,7));
+  matrix.print('G');
+  matrix.setTextColor(matrix.Color333(4,0,7)); 
+  matrix.print("B");
+  matrix.setTextColor(matrix.Color333(7,0,4)); 
+  matrix.print("*");
 
 
 
